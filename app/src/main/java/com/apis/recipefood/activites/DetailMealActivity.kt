582e1682caa30c5bf51da25798edb40b388adeb4
sofia aguilar <sofia.aguilar@itscordoba.edu.ar>
2022-08-18 18:57:45 -0300

    progressbar y link youtube

--- app/src/main/java/com/apis/recipefood/activites/DetailMealActivity.kt
+++ app/src/main/java/com/apis/recipefood/activites/DetailMealActivity.kt
@@ -2,8 +2,11 @@ package com.apis.recipefood.activites
 
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import com.apis.recipefood.constants.Constants.Companion.MEAL_ID
 import com.apis.recipefood.constants.Constants.Companion.MEAL_NAME
@@ -18,6 +21,7 @@ class DetailMealActivity : AppCompatActivity() {
     private lateinit var mealId:String
     private lateinit var mealName:String
     private lateinit var mealThumb:String
+    private lateinit var youtubeLink:String
     private val mealMvvm:MealViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,18 +32,32 @@ class DetailMealActivity : AppCompatActivity() {
         getRandomMealInformation()
         setInformationInView()
 
+        loadingCase()
         mealMvvm.getDetailMeal(mealId)
         observerDetailMeal()
-    }
 
+        onYoutubeImageClick()
+    }
+    //Cuando hago click en imagen de youtube
+    private fun onYoutubeImageClick() {
+
+        binding.ivYoutube.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeLink))
+            startActivity(intent)
+        }
+
+    }
 
     @SuppressLint("SetTextI18n")
     private fun observerDetailMeal() {
         mealMvvm.observerDetailMealLiveData().observe(this
         ) { t ->
+            onResponseCase()
             binding.tvCategory.text = "Categoría: ${t!!.strCategory}"
             binding.tvOrigen.text = "Origen: ${t.strArea}"
             binding.tvContenido.text = t.strInstructions
+
+            youtubeLink = t.strYoutube
 
         }
     }
@@ -50,6 +68,8 @@ class DetailMealActivity : AppCompatActivity() {
         Glide.with(applicationContext)
             .load(mealThumb)
             .into(binding.ivMealDetail)
+
+        binding.collapsingToolbar.title= mealName
     }
 
     //Obtengo informacion de la comida aleatoria
@@ -59,4 +79,32 @@ class DetailMealActivity : AppCompatActivity() {
         mealName = intent.getStringExtra(MEAL_NAME)!!
         mealThumb = intent.getStringExtra(MEAL_THUMB)!!
     }
+
+    //Cuando la imagen de la api este cargando
+    private fun loadingCase(){
+        //Mostrar
+        binding.progressBar.visibility = View.VISIBLE
+        //Ocultar
+        binding.btnFavorite.visibility = View.INVISIBLE
+        binding.tvInstrucciones.visibility = View.INVISIBLE
+        binding.tvCategory.visibility = View.INVISIBLE
+        binding.tvOrigen.visibility = View.INVISIBLE
+        binding.ivYoutube.visibility = View.INVISIBLE
+
+    }
+
+    //Cuando la api ya cargo la imagen correctamente
+    private fun onResponseCase(){
+        //Ocultar
+        binding.progressBar.visibility = View.INVISIBLE
+       //Mostrar
+        binding.btnFavorite.visibility = View.VISIBLE
+        binding.tvInstrucciones.visibility = View.VISIBLE
+        binding.tvCategory.visibility = View.VISIBLE
+        binding.tvOrigen.visibility = View.VISIBLE
+        binding.ivYoutube.visibility = View.VISIBLE
+
+    }
+
+
 }
